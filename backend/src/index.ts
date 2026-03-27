6bcb5c516c674ba5273025f22ce5ed4c2ad5f7e5
JoaoManoelAlves <joaomanoel88642@gmail.com>
2026-03-27 16:15:32 -0300

    Construção Inicial da rota de login e declaração do loginType

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,15 @@
 import Fastify from 'fastify'
-
+import {FastifyRequest, FastifyReply} from 'fastify'
+import { UserLogin } from './types/loginType.js'
 const app = Fastify()
 
 app.get('/', async (request, reply) => {
   reply.send("hello world")
 })
 
+app.post('/login', (request: FastifyRequest, reply: FastifyReply) => {
+
+})
 app.listen({ port: 3000 }, (err, address) => {
   if (err) {
     console.error(err)
